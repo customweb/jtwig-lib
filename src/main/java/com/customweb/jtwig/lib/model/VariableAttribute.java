1bdcdfdec8d42012f4263762bbd5ab1d41c968a9
Simon Schurter <simon.schurter@customweb.com>
2014-11-25 18:28:53 +0100

    Add method to get typed variable from variable attribute.

--- src/main/java/com/customweb/jtwig/lib/model/VariableAttribute.java
+++ src/main/java/com/customweb/jtwig/lib/model/VariableAttribute.java
@@ -19,9 +19,17 @@ public class VariableAttribute extends ValueAttribute {
 
 	public Object getVariable(RenderContext context) {
 		Object value = context.map(this.getValue());
-		if (value.equals(Undefined.UNDEFINED)) {
-			throw new RuntimeException("The variable for attribute '" + this.getKey() + "' has not been set,");
+		if (value == null || value.equals(Undefined.UNDEFINED)) {
+			throw new RuntimeException("The variable for attribute '" + this.getKey() + "' has not been set.");
 		}
 		return value;
+	}
+	
+	public <T> T getVariable(RenderContext context, Class<T> type) {
+		Object value = this.getVariable(context);
+		if (!type.isInstance(value)) {
+			throw new RuntimeException("The variable for attribute '" + this.getKey() + "' is not of type " + type.getCanonicalName() + ".");
+		}
+		return type.cast(value);
 	}
 }
