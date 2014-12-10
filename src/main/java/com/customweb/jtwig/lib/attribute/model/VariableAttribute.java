8fd7fb9baca9086c9bccdc0be90edd3a0878849b
Simon Schurter <simon.schurter@customweb.com>
2014-12-10 13:15:35 +0100

    Improve variable attribute.

--- src/main/java/com/customweb/jtwig/lib/attribute/model/VariableAttribute.java
+++ src/main/java/com/customweb/jtwig/lib/attribute/model/VariableAttribute.java
@@ -5,10 +5,12 @@ import com.lyncode.jtwig.expressions.api.CompilableExpression;
 import com.lyncode.jtwig.parser.config.ParserConfiguration;
 import com.lyncode.jtwig.render.RenderContext;
 import com.lyncode.jtwig.types.Undefined;
+import com.lyncode.jtwig.util.ObjectExtractor;
+import com.lyncode.jtwig.util.ObjectExtractor.ExtractException;
 
 public class VariableAttribute extends ValueAttribute {
 	private Object variable;
-	
+
 	public VariableAttribute(CompilableExpression key, CompilableExpression value, ParserConfiguration configuration) {
 		super(key, value, configuration);
 	}
@@ -16,7 +18,7 @@ public class VariableAttribute extends ValueAttribute {
 	public VariableAttribute(String key, CompilableExpression value, ParserConfiguration configuration) {
 		super(key, value, configuration);
 	}
-	
+
 	public VariableAttribute(String key, String value) {
 		super(key, value);
 	}
@@ -27,7 +29,7 @@ public class VariableAttribute extends ValueAttribute {
 		}
 		return this.variable;
 	}
-	
+
 	public <T> T getVariable(Class<T> type) {
 		return type.cast(this.getVariable());
 	}
@@ -35,10 +37,34 @@ public class VariableAttribute extends ValueAttribute {
 	@Override
 	public void render(RenderContext context) throws RenderException {
 		super.render(context);
+
+		String path = this.getValue();
+		String beanName;
+		String expression;
+		Object variable = null;
+
+		int dotPos = path.indexOf('.');
+		if (dotPos == -1) {
+			beanName = path;
+			expression = null;
+		} else {
+			beanName = path.substring(0, dotPos);
+			expression = path.substring(dotPos + 1);
+		}
 		
-		Object variable = context.map(this.getValue());
-		if (variable == null || variable.equals(Undefined.UNDEFINED)) {
-			throw new RuntimeException("The variable for attribute '" + this.getKey() + "' has not been set.");
+		Object target = context.map(beanName);
+		if (target == null || target.equals(Undefined.UNDEFINED)) {
+			throw new IllegalStateException("No target object for bean name '" + beanName + "' available as model map attribute.");
+		}
+		if (expression != null) {
+			ObjectExtractor extractor = new ObjectExtractor(target);
+			try {
+				variable = extractor.extract(expression);
+			} catch (ExtractException e) {
+				throw new RenderException(e);
+			}
+		} else {
+			variable = target;
 		}
 		this.variable = variable;
 	}
